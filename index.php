92ebb279eebdfb09199e80e886ff29d852de5c32
alexandrkut <kn1ght@kn1ght.ru>
2017-10-19 14:35:47 +0300

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿
+<?php
+include "header.php";
+?>
+<body onload='get("events")'>
+<div id="main">
+<div>
+<a href="#" onclick="get('events');">События</a>
+<a href="#" onclick="get('peoples');">Люди</a>
+<a href="edit_events.php" >Редактор событий</a>
+<a href="edit_peoples.php" >Редактор спортсменов</a>
+<hr>
+</div>
+
+<div id="date_table"></div>
+</div>
+
+
